18fc186d0e98f9797462f303f9dbbe979f9b1178
git-rob8 <166199755+git-rob8@users.noreply.github.com>
2024-04-06 11:31:09 +0530

    lab_07

---
+++ lab_07/01arithmetic_operation.sh
@@ -0,0 +1,15 @@
+echo Enter two numbers to add 
+read a b
+echo The addition of two number is $((a+b))
+
+echo Enter two numbers to subtract 
+read a b
+echo The addition of two number is $((a-b))
+
+echo Enter two numbers to multiply 
+read a b
+echo The addition of two number is $((a*b))
+
+echo Enter two numbers to divide 
+read a b
+echo The addition of two number is $((a/b))
---
+++ lab_07/02simple_interest.sh
@@ -0,0 +1,9 @@
+echo Enter the principle value 
+read p
+echo Enter rate of interest 
+read r
+echo Enter time period 
+read n
+i=`expr $p \* $n \* $r / 100` 
+echo "The simple interest is " 
+echo $i
